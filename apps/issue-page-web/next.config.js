95fe16ef7472a8298e6748fabd730a1af534c4a8
himyjan <51815522+himyjan@users.noreply.github.com>
2022-10-07 11:23:26 +0800

    upload unocss for next js example

--- apps/issue-page-web/next.config.js
+++ apps/issue-page-web/next.config.js
@@ -1,6 +1,13 @@
+const UnoCSS = require('@unocss/webpack').default;
+
 const withTM = require('next-transpile-modules')(['ui']);
 
 module.exports = withTM({
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.cache = false;
+    config.plugins.push(UnoCSS());
+    return config;
+  },
 });
